33881fd657689dcea3005cb27fd99895bed19ff8
DarrenCT <99516347+DarrenCT@users.noreply.github.com>
2023-11-21 09:06:32 -0500

    Added position placement on gameboard

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class TicTacToe {
 
     public static void main(String[] args) {
@@ -8,7 +10,46 @@ public class TicTacToe {
                 { '-', '+', '-', '+', '-' },
                 { ' ', '|', ' ', '|', ' ' }, };
         printGameBoard(gameBoard);
+        // Scan user input, prompt for input between 1-9
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter your placement (1-9)");
+        int position = scan.nextInt();
 
+        System.out.println(position);
+
+        switch (position) {
+            case 1:
+                gameBoard[0][0] = 'X';
+                break;
+            case 2:
+                gameBoard[0][2] = 'X';
+                break;
+            case 3:
+                gameBoard[0][4] = 'X';
+                break;
+            case 4:
+                gameBoard[2][0] = 'X';
+                break;
+            case 5:
+                gameBoard[2][2] = 'X';
+                break;
+            case 6:
+                gameBoard[2][4] = 'X';
+                break;
+            case 7:
+                gameBoard[4][0] = 'X';
+                break;
+            case 8:
+                gameBoard[4][2] = 'X';
+                break;
+            case 9:
+                gameBoard[4][4] = 'X';
+                break;
+            default:
+                break;
+        }
+
+        printGameBoard(gameBoard);
     }
 
     public static void printGameBoard(char[][] gameBoard) {
